d81ef95cdca07aa5839a070b8d0f4709a584d12d
Byambatsog <byambatsog@brilent.com>
2016-12-04 12:24:55 -0600

    made changes in loginResource

--- src/main/java/com/comp344/ecommerce/service/workflow/LoginActivity.java
+++ src/main/java/com/comp344/ecommerce/service/workflow/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.comp344.ecommerce.service.workflow;
 
+import com.comp344.ecommerce.business.CustomerManager;
+import com.comp344.ecommerce.business.LoginManager;
+import com.comp344.ecommerce.business.PartnerManager;
+import com.comp344.ecommerce.domain.Customer;
+import com.comp344.ecommerce.domain.Login;
+import com.comp344.ecommerce.domain.LoginRole;
+import com.comp344.ecommerce.domain.Partner;
+import com.comp344.ecommerce.jwt.AuthorizationUtil;
 import com.comp344.ecommerce.jwt.TokenAuthenticationService;
 import com.comp344.ecommerce.jwt.UserAuthentication;
 import com.comp344.ecommerce.jwt.UserService;
+import com.comp344.ecommerce.service.representation.BaseRepresentation;
 import com.comp344.ecommerce.service.representation.CustomerRepresentation;
 import com.comp344.ecommerce.service.representation.LoginRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
@@ -33,6 +44,18 @@ public class LoginActivity {
     @Autowired
     private TokenAuthenticationService tokenAuthenticationService;
 
+    @Autowired
+    private AuthorizationUtil authorizationUtil;
+
+    @Autowired
+    private LoginManager loginManager;
+
+    @Autowired
+    private CustomerManager customerManager;
+
+    @Autowired
+    private PartnerManager partnerManager;
+
     public LoginRepresentation login( String username, String password, HttpServletResponse response) throws Exception {
 
         try{
@@ -47,7 +70,23 @@ public class LoginActivity {
         UserAuthentication authentication = new UserAuthentication(user);
 
         LoginRepresentation loginRepresentation = new LoginRepresentation(user.getUsername(), tokenAuthenticationService.addAuthentication(response, authentication));
-
+        setLink(loginRepresentation, authentication);
         return loginRepresentation;
+    }
+
+    private void setLink(LoginRepresentation loginRepresentation, UserAuthentication authentication) {
+        Login login = loginManager.findByEmail((String)authentication.getPrincipal());
+        if(login.getRole().equals(LoginRole.ROLE_CUSTOMER)){
+            Customer customer = customerManager.findByLogin(login.getId());
+            loginRepresentation.addLink(BaseRepresentation.BASE_URI + "/customerservice/customers/" + customer.getId(),
+                    "customer", HttpMethod.GET, "");
+        }
+
+        if(login.getRole().equals(LoginRole.ROLE_PARTNER)){
+            Partner partner = partnerManager.findByLogin(login.getId());
+            loginRepresentation.addLink(BaseRepresentation.BASE_URI + "/partnerservice/partners/" + partner.getId(),
+                    "partner", HttpMethod.GET, "");
+        }
+
     }
 }
